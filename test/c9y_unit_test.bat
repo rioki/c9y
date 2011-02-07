5309a47c53f104188a12f2b26e2c3b551859f9ff
Sean Farrell <sean.farrell@rioki.org>
2011-02-07 21:18:42 +0100

    Adds copyleft.

--- test/c9y_unit_test.bat
+++ test/c9y_unit_test.bat
@@ -1,3 +1,22 @@
+rem
+rem c9y - concurrency
+rem Copyright 2011 Sean Farell
+rem
+rem This file is part of c9y.
+rem
+rem c9y is free software: you can redistribute it and/or modify
+rem it under the terms of the GNU Lesser General Public License as published by
+rem the Free Software Foundation, either version 3 of the License, or
+rem (at your option) any later version.
+rem
+rem c9y is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem GNU General Public License for more details.
+rem
+rem You should have received a copy of the GNU Lesser General Public License
+rem along with c9y. If not, see <http://www.gnu.org/licenses/>.
+rem
 
 set TARGET_DIR=%1
 set SIGC_DIR=%2

42abf7af8960ff1f6924bb1bf1233f0c5538d594
Sean Farrell <sean.farrell@rioki.org>
2011-06-06 21:36:03 +0200

    Changes project layout to follow common scheme.

--- deploy.bat
+++
@@ -1,6 +0,0 @@
-
-set CONFIG=%1
-
-xcopy /Y c9y\%CONFIG%\pikazo.dll %C9Y_DIR%\bin 
-xcopy /Y c9y\%CONFIG%\pikazo.dll %C9Y_DIR%\lib
-xcopy /Y c9y\*.h %C9Y_DIR%\include\c9y
--- run_test.bat
+++ run_test.bat
@@ -1,7 +1,6 @@
 
 set CONFIG=%1
 
-xcopy /Y ..\c9y\%CONFIG%\*.dll %CONFIG%
 xcopy /Y %SIGC_DIR%\bin\*.dll %CONFIG%
 
 %CONFIG%\c9y_test.exe

e4c83551e8b51fd77dfd921606c08b4b865fd326
Sean Farrell <sean.farrell@rioki.org>
2017-12-15 12:47:26 -0800

    Documents thread

--- c9y/thread_pool.h
+++ c9y/thread_pool.h
@@ -32,25 +32,64 @@
 
 namespace c9y
 {
+    /**
+     * Thread Pool
+     **/
     class C9Y_EXPORT thread_pool
     {
     public:
+        /**
+         * Contruct empty thread pool.
+         * 
+         * The default constructor will create a thread pool, but with no threads.
+         * It can be used to create an instance on the stack that can later accept
+         * a running thread pool with the move assignment.
+         **/
         thread_pool() noexcept;
-
+        
+        /**
+         * Create a thread pool.
+         * 
+         * @param thread_func The thread function each thread in the pool executes.
+         * @param concurenct The number of threads to run concurently.
+         * 
+         * This constructor will create @arg concurency threads that execute @arg thread_func.
+         **/
         thread_pool(std::function<void ()> thread_func, size_t concurency);
 
         thread_pool(const thread_pool&) = delete;
-
+        
+        /**
+         * Move Constructor
+         **/
         thread_pool(thread_pool&& other) noexcept;
-
+        
+        /** 
+         * Destructor
+         *
+         * @warning If the thread pool is not joined, the underlying std::thread
+         * objects will invoke std::terminate.
+         **/
         ~thread_pool();
 
         thread_pool& operator = (const thread_pool&) = delete;
-
+        
+        /**
+         * Move Asignment
+         **/
         thread_pool& operator = (thread_pool&& other) noexcept;
-
+        
+        /**
+         * Join each thread in the pool.
+         *
+         * Calling join ensures that each thread function terminates and
+         * the coresponsing memory is freed.
+         **/
         void join();
 
+        /**
+         * Swap thread pool.
+         **/
         void swap(thread_pool& other) noexcept;
 
     private:

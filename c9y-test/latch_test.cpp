87508523fc5a762af8143e9d6055153ed315d9f8
Sean Farrell <sean.farrell@rioki.org>
2021-05-27 13:38:32 +0200

    Adds latch.

---
+++ c9y-test/latch_test.cpp
@@ -0,0 +1,46 @@
+//
+// c9y - concurrency
+// Copyright(c) 2017-2021 Sean Farrell
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#include <c9y/latch.h>
+
+#include <atomic>
+#include <gtest/gtest.h>
+
+#include <c9y/async.h>
+
+TEST(latch, wait)
+{
+    auto my_latch = c9y::latch{12u};
+    auto count    = std::atomic<unsigned int>{0u};
+
+    for (auto i = 0u; i < 12; i++)
+    {
+        c9y::async([&] () {
+            my_latch.count_down();
+            count++;
+        });
+    }
+
+    my_latch.wait();
+    EXPECT_EQ(12u, count);
+}
---
+++ c9y/latch.cpp
@@ -0,0 +1,60 @@
+//
+// c9y - concurrency
+// Copyright(c) 2017-2021 Sean Farrell
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#include "latch.h"
+
+namespace c9y
+{
+    std::ptrdiff_t latch::max() noexcept
+    {
+        return std::numeric_limits<std::ptrdiff_t>::max();
+    }
+
+    latch::latch(std::ptrdiff_t expected)
+    : count(expected) {}
+
+    void latch::count_down(std::ptrdiff_t n)
+    {
+        auto lock = std::unique_lock<std::mutex>{mutex};
+        count -= n;
+        if (count <= 0)
+        {
+            cond.notify_all();
+        }
+    }
+
+    bool latch::try_wait() const noexcept
+    {
+        wait();
+        return true;
+    }
+
+    void latch::wait() const
+    {
+        auto lock = std::unique_lock<std::mutex>{mutex};
+        if (count > 0)
+        {
+            cond.wait(lock);
+        }
+    }
+}

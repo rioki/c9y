95ef26a0048f286a855ac1b9ee97d07c55403af7
Sean Farrell <sean.farrell@rioki.org>
2022-12-02 09:28:08 +0100

    Add threadsafe death test.
    We actually don't need it, but this is the better option and
    shuts up gtest. It's slower... but at a total test time of 1s,
    what can go wrong?

--- c9y-test/main.cpp
+++ c9y-test/main.cpp
@@ -29,5 +29,6 @@ int main(int argc, char* argv[])
 {
     std::srand(std::time(nullptr));
     testing::InitGoogleTest(&argc, argv);
+    GTEST_FLAG_SET(death_test_style, "threadsafe");
     return RUN_ALL_TESTS();
 }

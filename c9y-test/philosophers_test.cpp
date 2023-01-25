2dd3f6f2263f23cfd50b467f11cdb54bbf2940f4
Sean Farrell <sean.farrell@rioki.org>
2023-01-25 11:35:38 +0100

    Add philosophers test.

---
+++ c9y-test/philosophers_test.cpp
@@ -0,0 +1,343 @@
+//
+// c9y - concurrency
+// Copyright 2017-2022 Sean Farrell
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#include <vector>
+#include <thread>
+#include <random>
+#include <chrono>
+
+#include <gtest/gtest.h>
+#include <c9y/c9y.h>
+
+enum class State
+{
+    INITIAL,
+    THINKING,
+    EATING
+};
+
+template <>
+struct std::formatter<State> : std::formatter<std::string>
+{
+    auto format(State value, format_context& ctx)
+    {
+        switch (value)
+        {
+        case State::INITIAL:
+            return formatter<string>::format("INITIAL", ctx);
+        case State::THINKING:
+            return formatter<string>::format("THINKING", ctx);
+        case State::EATING:
+            return formatter<string>::format("EATING", ctx);
+        default:
+            assert(false);
+            return formatter<string>::format("", ctx);
+        };
+    }
+};
+
+template <typename T>
+struct std::formatter<std::atomic<T>> : std::formatter<T>
+{
+    auto format(const std::atomic<T>& value, format_context& ctx)
+    {
+        return formatter<T>::format(value.load(), ctx);
+    }
+};
+
+void print(const size_t phi, const State state, const int satiation, const int health)
+{
+    static std::mutex cout_mutex; // prevent cout too tear
+    std::scoped_lock<std::mutex> ls(cout_mutex);
+
+    if (health < 1)
+    {
+        std::cout << std::format("Philosopher {} is dead. [s: {} h: {}]", phi, satiation, health) << std::endl;;
+        return;
+    }
+
+    switch (state)
+    {
+    case State::INITIAL:
+        std::cout << std::format("Philosopher {} woke up. [s: {} h: {}]", phi, satiation, health) << std::endl;
+        return;
+    case State::THINKING:
+        std::cout << std::format("Philosopher {} is thinking. [s: {} h: {}]", phi, satiation, health) << std::endl;
+        return;
+    case State::EATING:
+        std::cout << std::format("Philosopher {} is eating. [s: {} h: {}]", phi, satiation, health) << std::endl;
+        return;
+    default:
+        assert(false);
+    }
+}
+
+namespace atomic
+{
+    struct Fork
+    {
+        std::atomic<bool> available = true;
+    };
+
+    struct Philosopher
+    {
+        size_t              id        = 0;
+        std::atomic<State>  state     = State::INITIAL;
+        std::atomic<int>    satiation = 0;
+        std::atomic<int>    health    = 100;
+
+        Philosopher(size_t _id)
+        : id(_id) {}
+
+        Philosopher(const Philosopher& rhs)
+        : id(rhs.id), state(rhs.state.load()), satiation(rhs.satiation.load()), health(rhs.health.load()) {}
+
+        void print()
+        {
+            ::print(id, state, satiation, health);
+        }
+
+        void think(unsigned int duration)
+        {
+            state = State::THINKING;
+            print();
+            std::this_thread::sleep_for(std::chrono::milliseconds(duration));
+        }
+
+        void eat(std::vector<Fork>& forks, unsigned int duration)
+        {
+            const auto left_fork = id;
+            const auto right_fork = (left_fork + 1) % forks.size();
+            ASSERT_LT(left_fork, forks.size());
+            ASSERT_LT(right_fork, forks.size());
+
+            auto left_available = forks[left_fork].available.load();
+            auto right_available = forks[right_fork].available.load();
+
+            if (!left_available || !right_available)
+            {
+                return;
+            }
+
+            if (forks[left_fork].available.compare_exchange_weak(left_available, false) == false)
+            {
+                // in the mean time someone else took it
+                return;
+            }
+
+            if (forks[right_fork].available.compare_exchange_weak(right_available, false) == false)
+            {
+                // give the left one back
+                forks[left_fork].available = true;
+                return;
+            }
+
+            state = State::EATING;
+            print();
+            std::this_thread::sleep_for(std::chrono::milliseconds(duration));
+            satiation += duration;
+
+            forks[left_fork].available = true;
+            forks[right_fork].available = true;
+        }
+
+    };
+}
+
+auto get_random_seed()
+{
+    static std::mutex m;
+    static std::random_device rd;
+
+    std::scoped_lock<std::mutex> ls(m);
+    return rd();
+}
+
+TEST(philosophers, NOCI_atomics)
+{
+    using namespace atomic;
+
+    std::vector<Philosopher> philosophers;
+    for (size_t i = 0u; i < std::thread::hardware_concurrency(); i++)
+    {
+        philosophers.emplace_back(i);
+    }
+
+    std::vector<Fork> forks(philosophers.size());
+
+    std::atomic<size_t> last_id = 0;
+    c9y::thread_pool pool([&] () {
+        try
+        {
+            std::mt19937 gen(get_random_seed());
+            std::uniform_int_distribution<> thinking_dist(10, 100);
+            std::uniform_int_distribution<> eating_dist(1, 25);
+
+            auto id = last_id++;
+            ASSERT_LT(id, philosophers.size());
+            philosophers[id].print();
+
+            while (philosophers[id].satiation < 100 && philosophers[id].health > 0 )
+            {
+                philosophers[id].think(thinking_dist(gen));
+                philosophers[id].eat(forks, eating_dist(gen));
+            }
+        }
+        catch (const std::exception& ex)
+        {
+            // No we dont actually expect it, but we want to see it.
+            EXPECT_EQ("Exception", ex.what());
+            ASSERT_TRUE(false);
+        }
+        catch (...)
+        {
+            ASSERT_TRUE(false);
+        }
+    }, philosophers.size());
+
+    pool.join();
+
+    for (const auto& philosopher : philosophers)
+    {
+        EXPECT_GT(philosopher.health, 0);
+        EXPECT_GE(philosopher.satiation, 100);
+    }
+}
+
+TEST(philosophers, NOCI_async_and_sync)
+{
+    c9y::set_main_thread_id(std::this_thread::get_id());
+
+    struct Philosopher
+    {
+        size_t           id;
+        State            state     = State::INITIAL;
+        std::atomic<int> satiation = 0;
+        std::atomic<int> health    = 100;
+
+        Philosopher(size_t _id)
+        : id(_id) {}
+
+        Philosopher(const Philosopher& rhs)
+        : id(rhs.id), state(rhs.state), satiation(rhs.satiation.load()), health(rhs.health.load()) {}
+    };
+    std::vector<Philosopher> philosophers;
+    for (size_t i = 0u; i < std::thread::hardware_concurrency(); i++)
+    {
+        philosophers.emplace_back(i);
+    }
+
+    struct Fork
+    {
+        bool available = true;
+    };
+    std::vector<Fork> forks(philosophers.size());
+
+    std::mt19937 gen(get_random_seed());
+    std::uniform_int_distribution<> thinking_dist(1, 25);
+    std::uniform_int_distribution<> eating_dist(25, 50);
+
+    auto print = [] (const Philosopher& phil)
+    {
+        c9y::sync([id = phil.id, state = phil.state, satiation = phil.satiation.load(), health = phil.health.load()]() mutable
+        {
+            std::cout << std::format("id: {}, state: {}, satiation: {}, health: {}", id, state, satiation, health) << std::endl;
+        });
+    };
+
+    auto think = [&] (Philosopher& phil) mutable
+    {
+        auto f = c9y::sync<std::chrono::milliseconds>([&] () mutable
+        {
+            auto ammount = thinking_dist(gen);
+            phil.state = State::THINKING;
+            phil.health -= ammount / 2;
+            return std::chrono::milliseconds(ammount);
+        });
+        print(phil);
+        std::this_thread::sleep_for(f.get());
+    };
+
+    auto eat = [&] (Philosopher& phil) mutable
+    {
+        auto f = c9y::sync<std::chrono::milliseconds>([&] () mutable
+        {
+            auto& left_fork = forks[phil.id];
+            auto& right_fork = forks[(phil.id + 1) % forks.size()];
+
+            if (left_fork.available && right_fork.available)
+            {
+                auto ammount    = eating_dist(gen);
+                phil.state      = State::EATING;
+                phil.satiation += ammount;
+                left_fork.available  = false;
+                right_fork.available = false;
+                return std::chrono::milliseconds(thinking_dist(gen));
+            }
+            return std::chrono::milliseconds(0);
+        });
+
+        print(phil);
+        auto duration = f.get();
+        std::this_thread::sleep_for(duration);
+
+        if (duration != std::chrono::milliseconds(0))
+        {
+            c9y::sync([id = phil.id, &forks] () mutable
+            {
+                auto& left_fork  = forks[id];
+                auto& right_fork = forks[(id + 1) % forks.size()];
+                left_fork.available  = true;
+                right_fork.available = true;
+            });
+        }
+    };
+
+    std::vector<std::future<bool>> results(philosophers.size());
+    for (auto i = 0u; i < philosophers.size(); i++)
+    {
+        results[i] = c9y::async<bool>([=] () mutable
+        {
+            print(philosophers[i]);
+
+            while (philosophers[i].satiation < 100)
+            {
+                if (philosophers[i].health < 0)
+                {
+                    return false;
+                }
+
+                think(philosophers[i]);
+                eat(philosophers[i]);
+            }
+            return true;
+        });
+    }
+
+    while (!c9y::all_ready(results))
+    {
+        c9y::sync_point();
+    }
+
+    c9y::wait_all(results);
+}

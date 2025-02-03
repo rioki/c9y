7515bdc84ad18229bc00f0ebec9c66b7ebb465a2
Sean Farrell <sean.farrell@rioki.org>
2025-02-03 10:59:24 +0100

    Adds defer. (#102)
    Defer is like delay, except it does not need any locks. delay and sync need to be redesigned.

---
+++ c9y-test/defer_test.cpp
@@ -0,0 +1,74 @@
+//
+// c9y - concurrency
+// Copyright 2017-2023 Sean Farrell
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#include <c9y/defer.h>
+
+#include <gtest/gtest.h>
+
+TEST(defer, empty_catch_up)
+{
+    c9y::catch_up();
+}
+
+TEST(defer, shedule_one_task)
+{
+    auto count = 0u;
+    c9y::defer([&] () {
+        count++;
+    });
+    EXPECT_EQ(count, 0u);
+
+    c9y::catch_up();
+    EXPECT_EQ(count, 1u);
+
+    c9y::catch_up();
+    EXPECT_EQ(count, 1u);
+}
+
+TEST(defer, shedule_multiple_tasks)
+{
+    auto count = 0u;
+    c9y::defer([&] () {
+        count++;
+    });
+    c9y::defer([&] () {
+        count++;
+    });
+    c9y::defer([&] () {
+        count++;
+    });
+    EXPECT_EQ(count, 0u);
+
+    c9y::catch_up();
+    EXPECT_EQ(count, 3u);
+
+    c9y::catch_up();
+    EXPECT_EQ(count, 3u);
+}
+
+TEST(defer, null_defer_throws)
+{
+    EXPECT_THROW(c9y::defer({}), std::invalid_argument);
+}
+
+
---
+++ c9y/defer.h
@@ -0,0 +1,46 @@
+// c9y - concurrency
+// Copyright 2017-2025 Sean Farrell
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef _C9Y_DEFER_H_
+#define _C9Y_DEFER_H_
+#include "defines.h"
+
+#include <functional>
+
+namespace c9y
+{
+    //! Schedule some work to be exectued on this thread later in catch_up.
+    //!
+    //! This function runs lockless and thus is quite efficient and does not
+    //! mess with other threads.
+    //!
+    //! @param fun the function the schedule
+    C9Y_EXPORT
+    void defer(std::function<void()> fun);
+
+    //! Exectue any pending work added by defer.
+    //!
+    //! @returns true if any work was executed.
+    C9Y_EXPORT
+    bool catch_up();
+}
+
+#endif
